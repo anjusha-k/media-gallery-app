f4cc776ebbb68344b751361cff9dee7f8c38f4d5
anjusha-k <anjusha.khandavalli@gmail.com>
2025-06-23 08:14:06 +1000

    Update next config to fix deployment

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   experimental: {
-    optimizePackageImports: ["@mantine/core", "@mantine/hooks"],
+    optimizePackageImports: ["@chakra-ui/react"],
   },
 };
 
